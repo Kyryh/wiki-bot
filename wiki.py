f99dec1440ec23fa84575d0a040b1846302b4716
Kyryh <71778401+Kyryh@users.noreply.github.com>
2024-06-27 22:25:32 +0200

    Wiki page content extraction

--- wiki.py
+++ wiki.py
@@ -1,9 +1,11 @@
 from httpx import AsyncClient
 import re
+from html import unescape as unescape_html
 
 class Wiki:
 
     SEARCH_REGEX = re.compile('<div class="mw-search-result-heading"><a href="(.*?)" title="(.*?)".*?<\/div>')
+    CONTENT_REGEX = re.compile("<textarea.*?>(.*?)<\/textarea>", re.S)
 
     def __init__(self, base_url: str):
         self.base_url = re.split("(?<!\W)/(?!\W)", base_url)[0]
@@ -20,3 +22,12 @@ class Wiki:
         )
         search_results = self.SEARCH_REGEX.findall(search_response.text)
         return {result[1]: result[0] for result in search_results}
+
+    async def get_page_content(self, path: str) -> str:
+        content = await self.client.get(
+            self.base_url + path,
+            params = {
+                "action": "edit"
+            }
+        )
+        return unescape_html(self.CONTENT_REGEX.search(content.text).group(1))
