a2d4f29a4557a5d703c01276ca61469c29ee2900
Kyryh <71778401+Kyryh@users.noreply.github.com>
2024-07-17 22:08:35 +0200

    Added a way to fix tags broken by textwrap

--- wiki.py
+++ wiki.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from httpx import AsyncClient
 import re
 from bs4 import BeautifulSoup
@@ -76,17 +77,43 @@ class Wiki:
                 if element.text and not element.text.isspace():
                     content[-1]["data"] += element.decode_contents()
 
-        return "\n\n".join(
+        return "\n".join(
             self._parse_section(section['heading'], section['data']) for section in content if section["data"] != ""
         )
     
     def _parse_section(self, heading: str, data: str) -> str:
         data = re.sub(r"<img\s.*?\/>", "", data)
         data = re.sub(r"<\/?(span|br).*?>", "", data)
-        data = data.replace("href=\"", "href=\"" + self.url)
+        data = re.sub(r"<a href=\"(.*?)\".*?>", fr'<a href="{self.url}\1">', data)
         data = data.replace("\xa0", "")
         return f"<b>{heading}</b>\n{data}"
+    
+    @staticmethod
+    def fix_tags_single(text: str) -> tuple[str, str]:
+        tags: list[str] = re.findall(r"<(.*?)>", text)
+        tags_count = defaultdict(int)
+        last_tags = defaultdict(list)
+        for tag in tags:
+            effective_tag = tag.replace("/", "").split(" ")[0]
+            if tag[0] != "/":
+                last_tags[effective_tag].append(tag)
+                tags_count[effective_tag] += 1
+            else:
+                tags_count[effective_tag] -= 1
 
-
-
+        next_text_prefix = ""
+        for tag, count in tags_count.items():
+            if count > 0:
+                text += "</"+tag*count+">"
+                next_text_prefix += "<"+last_tags[tag].pop()+">"
+        
+        return text, next_text_prefix
+    
+    @staticmethod
+    def fix_tags_multiple(texts: list[str]) -> list[str]:
+        new_texts = list(texts)
+        text_prefix = ""
+        for i in range(len(new_texts)):
+            new_texts[i], text_prefix = Wiki.fix_tags_single(text_prefix+new_texts[i])
+        return new_texts
 
